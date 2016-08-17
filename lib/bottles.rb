11f4f54dd33b24bd7d0f4fee1eff3c9648d148db
Raphaela Wrede <raphaela.wrede@gmail.com>
2016-08-17 11:58:07 +0200

    implement verse(2)

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,8 +3,14 @@ class Bottles
   def verse(number)
     <<-VERSE
 #{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+Take one down and pass it around, #{number - 1} #{singular_or_plural(number - 1)} of beer on the wall.
     VERSE
+  end
+
+  private
+
+  def singular_or_plural(number)
+    number < 2 ? "bottle" : "bottles"
   end
 
 end
