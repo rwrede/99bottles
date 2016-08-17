e4d32e98ee3e3769c8b8eb6d709b851d485bf4e2
Raphaela Wrede <raphaela.wrede@gmail.com>
2016-08-17 10:44:08 +0200

    implements verse(99)

---
+++ lib/bottles.rb
@@ -0,0 +1,10 @@
+class Bottles
+
+  def verse(number)
+    <<-VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+    VERSE
+  end
+
+end
