3cbc1954091834a38033d82e048049fc52e23170
Raphaela Wrede <raphaela.wrede@gmail.com>
2016-08-18 10:53:20 +0200

    extract Line classes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -32,23 +32,86 @@ end
 
 class Verse
 
-  def initialize(bottles)
-    @bottles = bottles
+  def initialize(initial_bottles)
+    @initial_bottles = initial_bottles
   end
 
   def to_s
     <<-VERSE
-#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
-Take one down and pass it around, #{bottles - 1} #{singular_or_plural} of beer on the wall.
+#{first_line}
+#{second_line}
     VERSE
+  end
+
+  private
+
+  attr_reader :initial_bottles
+
+  def first_line
+    FirstLine.new(initial_bottles).to_s
+  end
+
+  def second_line
+    SecondLine.new(initial_bottles - 1).to_s
+  end
+
+  def singular_or_plural
+    (initial_bottles - 1) < 2 ? "bottle" : "bottles"
+  end
+
+end
+
+class Line
+
+  def initialize(bottles)
+    @bottles = bottles
   end
 
   private
 
   attr_reader :bottles
 
-  def singular_or_plural
-    (bottles - 1) < 2 ? "bottle" : "bottles"
+  def humanized_bottles
+    if bottles == 1
+      "bottle of beer"
+    else
+      "bottles of beer"
+    end
+  end
+
+  def humanized_number
+    if bottles == 0
+      "no more"
+    else
+      bottles.to_s
+    end
+  end
+
+end
+
+class FirstLine < Line
+
+  def to_s
+    "#{humanized_number.capitalize} #{humanized_bottles} on the wall, #{humanized_number} #{humanized_bottles}."
+  end
+
+end
+
+class SecondLine < Line
+
+  def to_s
+    [action, remaining_bottles].join(", ")
+  end
+
+  private
+
+  def action
+    subject = bottles == 0 ? "it" : "one"
+    "Take #{subject} down and pass it around"
+  end
+
+  def remaining_bottles
+    "#{humanized_number} #{humanized_bottles} on the wall."
   end
 
 end
